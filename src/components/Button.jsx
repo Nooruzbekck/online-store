fb27ba2c2c10494a43d4db2c98823b97e69e90ed
unknown <yasinabdulaev800@gmail.com>
2024-10-10 20:35:59 +0300

    finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,10 +12,6 @@ export const Button = ({ onClick, type, disabled, children, ...props }) => {
 const StyledButton = styled.button`
   width: 270px;
   height: 41px;
-  top: 209px;
-  gap: 0px;
-  border-radius: 0px 0px 4px 4px;
-  opacity: 0px;
 
   font-size: 16px;
   font-weight: 500;
