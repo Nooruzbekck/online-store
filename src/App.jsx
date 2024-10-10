6b31e8c09bf6ecca37d66147461bac7e519ea691
aisuluu <aisuluubekeeva.0555@gmail.com>
2024-10-10 19:30:04 +0300

    finished icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,1 @@
-export function App() {
-  return <div></div>;
-}
+
---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import Favorite from "./icons/favorite-isons.svg?react";
+import Basket from "./icons/basket-icons.svg?react";
+import Contact from "./icons/contact-icons.svg?react";
+import Facebook from "./icons/facebook-icons.svg?react";
+import Greystar from "./icons/greystar-icons.svg?react";
+import Head from "./icons/head-icons.svg?react";
+import Star from "./icons/star-icons.svg?react";
+import Youtybe from "./icons/youtube-icons.svg?react";
+import Inkeding from "./icons/inkedin-icons.svg?react";
+import Starlink from "./icons/starlink-icons.svg?react";
+import Instagram from "./icons/instagram-icons.svg?react"
+
+
+export const Icons = {
+ 
+  Favorite,
+  Basket,
+  Contact,
+  Facebook,
+  Greystar,
+  Head,
+  Star,
+  Youtybe,
+  Starlink,
+  Inkeding,
+  Instagram,
+};
